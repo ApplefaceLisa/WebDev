fb56c88d736fb8cfcc33569afd7e2c7c51c3fe3e
ApplefaceLisa <applefacelisa@gmail.com>
2017-03-23 23:12:26 -0700

    Update script.js

--- FreeCodeCamp/Frontend Projects/Adv02_Pomodoro-Clock/script.js
+++ FreeCodeCamp/Frontend Projects/Adv02_Pomodoro-Clock/script.js
@@ -1,170 +1,164 @@
 var pomodoro = {
     started: false,
-    status: "session",     // should be "session" or "break"
-    break: {
-      len: 5,
-      hrs: 0,
-      mins: 5,
-      secs: 0,
-      displayDom: null,
-      minusDom: null,
-      addDom: null
-    },
-
-    session: {
-      len: 25,
-      hrs: 0,
-      mins: 25,
-      secs: 0,
-      displayDom: null,
-      minusDom: null,
-      addDom: null
-    },
-
-    fillerHeight: 0,
-    fillerIncrement: 0,
+    status: "session",
     interval: null,
-
-    timerDom: null,
-    hoursDom: null,
-    minutesDom: null,
-    secondsDom: null,
+    break: {
+        len : 5,
+        displayDom : null
+    },
+    session: {
+        len : 25,
+        displayDom: null
+    },
+    // counter variables
+    hrs: 0,
+    mins: 25,
+    secs: 0,
+    hrDom: null,
+    minDom: null,
+    secDom: null,
     statusDom: null,
+    timeDom: null,
+
+    // filler variables
+    fillerHeight: 0,
+    fillerIncr: 0,
     fillerDom: null,
 
+    // methods
+    // format time display, always show 2 digits
     format: function(t) {
       return ("0"+t).slice(-2);
     },
 
-    // init break and session
-    initBS: function(self, vn) {   // vn should be "break" or "session"
-      self[vn].displayDom = document.getElementById(vn);
-      self[vn].minusDom = document.getElementById(vn+"Minus");
-      self[vn].addDom = document.getElementById(vn+"Add");
-    },
-
-    updateDom: function(vn) {
-        if (this.status === vn) {
-          var obj = this[this.status];
-          this.statusDom.textContent = vn.toUpperCase();
-          this.hoursDom.textContent = (obj.hrs > 0 ? (this.format(obj.hrs)+":") : "");
-          this.minutesDom.textContent = this.format(obj.mins);
-          this.secondsDom.textContent = this.format(obj.secs);
-          // update filler
-          this.fillerHeight = this.fillerHeight + this.fillerIncrement;
-          this.fillerDom.style.height = this.fillerHeight + 'px';
-        }
-    },
-
-    len2hms: function() {
-      var len = this.len;
+    // convert len to hr/min/sec
+    len2hms: function(len) {
       var h = Math.floor(len / 60);
       this.hrs = h > 0 ? h : 0;
       this.mins = len % 60;
       this.secs = 0;
     },
 
+    updateTimerDom: function() {
+        this.hrDom.textContent = (this.hrs > 0 ? (this.format(this.hrs)+":") : "");
+        this.minDom.textContent = this.format(this.mins);
+        this.secDom.textContent = this.format(this.secs);
+    },
+
+    toggleStatus: function() {
+        if (this.status === "session") {
+            this.status = "break";
+        } else {
+            this.status = "session";
+        }
+    },
+
+    updateStatusDom: function() {
+        this.statusDom.textContent = this.status;
+    },
+
+    adjustLen: function(status, step) {
+        if (this.started) {
+            return;
+        }
+        else {
+            var len = this[status].len;
+            if (len > 1 && len < 1440) {
+                this[status].len += step;
+                this[status].displayDom.textContent = this[status].len;
+            }
+            if (this.status === status) {
+                this.len2hms(this[status].len);
+                this.updateTimerDom();
+                this.resetFiller();
+            }
+        }
+    },
+
     resetFiller: function() {
-      this.fillerIncrement = 200/(this[this.status].len*60);
+      this.fillerIncrement = 300/(this[this.status].len*60);
       this.fillerHeight = 0;
+      this.fillerDom.style.backgroundColor
+        = (this.status === "session") ? "#99CC00" : "#FF4444";
     },
 
-    resetTimer: function(vn, step) {
-      if (!this.started) {
-        var obj = this[vn];
-        obj.len += step;
-        var len = obj.len;
-        obj.displayDom.value = len;
-
-        this.len2hms.call(obj);
-        this.resetFiller();
-        this.updateDom.call(this, vn);
-      }
+    increFiller: function() {
+        this.fillerHeight = this.fillerHeight + this.fillerIncrement;
     },
 
-    switchStatus: function() {
-      if (this.status === "session") {
-        this.status = "break";
-        this.fillerDom.style.backgroundColor = "#f4d341";
-      } else {
-        this.status = "session";
-        this.fillerDom.style.backgroundColor = "#99CC00";
-      }
+    updateFillerDom: function() {
+        this.fillerDom.style.height = this.fillerHeight + 'px';
     },
 
-    timerCountDown: function() {
-      var obj = this[this.status];
-      if (obj.secs == 0) {
-        if (obj.mins == 0) {
-          if (obj.hrs == 0) {
-            this.switchStatus();
-            this.len2hms.call(obj);
+    countDown:function() {
+      if (this.secs == 0) {
+        if (this.mins == 0) {
+          if (this.hrs == 0) {
+            this.toggleStatus();
+            this.updateStatusDom();
+            this.len2hms(this[this.status].len);
+            this.updateTimerDom();
             this.resetFiller();
+            this.updateFillerDom();
+            return;
           } else {
-            --obj.hrs;
-            obj.mins = 59;
-            obj.secs = 59;
+            --this.hrs;
+            this.mins = 59;
+            this.secs = 59;
           }
         } else {
-          --obj.mins;
-          obj.secs = 59;
+          --this.mins;
+          this.secs = 59;
         }
       } else {
-        --obj.secs;
+        --this.secs;
       }
-      this.updateDom(this.status);
+      this.updateTimerDom();
+      this.increFiller();
+      this.updateFillerDom();
     },
 
     init: function() {
-      var self = this;
-      this.timerDom = document.getElementById("timer");
-      this.hoursDom = document.getElementById("hours");
-      this.minutesDom = document.getElementById('minutes');
-      this.secondsDom = document.getElementById('seconds');
-      this.fillerDom = document.getElementById('filler');
-      this.statusDom = document.getElementById("status");
+        var self = this;
+        this.break.displayDom = document.getElementById("break");
+        this.session.displayDom = document.getElementById("session");
+        this.hrDom = document.getElementById("hours");
+        this.minDom = document.getElementById("minutes");
+        this.secDom = document.getElementById("seconds");
+        this.statusDom = document.getElementById("status");
+        this.timeDom = document.getElementById("time");
+        this.fillerDom = document.getElementById("filler");
 
-      this.initBS(self, "break");
-      this.initBS(self, "session");
-      this.resetFiller();
+        this.resetFiller();
 
-      this.break.minusDom.onclick = function() {
-        if (self.break.len > 1) {
-          self.resetTimer.call(self, "break", -1);
+        // attach event handler for buttons
+        /*
+        var buttons = Array.from(document.getElementsByTagName("button"));
+        for (var i = 0; i < buttons.length; i++) {
+            var status = buttons[i].parentElement.className;
+            var step = buttons[i].className === "minus" ? -1 : 1;
+            buttons[i].onclick = this.adjustLen.bind(self, status, step);
         }
-      }
+        */
+        Array.from(document.getElementsByTagName("button")).forEach(function(obj) {
+            var status = obj.parentElement.className;
+            var step = obj.className === "minus" ? -1 : 1;
+            obj.onclick = this.adjustLen.bind(this,status, step);
+        }, self);
 
-      this.break.addDom.onclick = function() {
-        if (self.break.len < 1440) {
-          self.resetTimer.call(self, "break", 1);
+        this.timeDom.onclick = function() {
+          self.started = !self.started;
+          if (!self.started) {
+            if (!self.interval) return;
+            clearInterval(self.interval);
+            self.interval = null;
+          } else {
+            self.interval = setInterval(function() {
+              self.countDown.apply(self);
+            }, 1000);
+          }
         }
-      }
-
-      this.session.minusDom.onclick = function() {
-        if (self.session.len > 1) {
-          self.resetTimer.call(self, "session", -1);
-        }
-      }
-
-      this.session.addDom.onclick = function() {
-        if (self.session.len < 1440) {
-          self.resetTimer.call(self, "session", 1);
-        }
-      }
-
-      this.timerDom.onclick = function() {
-        this.started = !this.started;
-        if (!this.started) {
-          if (!this.interval) return;
-          clearInterval(this.interval);
-          this.interval = null;
-        } else {
-          this.interval = setInterval(function() {
-            self.timerCountDown.apply(self);
-          }, 1000);
-        }
-      }
-    }
+    },
 }
 
 window.onload = function() {
