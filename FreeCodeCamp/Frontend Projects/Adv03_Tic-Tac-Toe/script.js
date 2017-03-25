ba90a25d0ef97ab1d545b53006afdd01ca3ff547
ApplefaceLisa <applefacelisa@gmail.com>
2017-03-25 11:10:37 -0700

    Update script.js

--- FreeCodeCamp/Frontend Projects/Adv03_Tic-Tac-Toe/script.js
+++ FreeCodeCamp/Frontend Projects/Adv03_Tic-Tac-Toe/script.js
@@ -1,13 +1,49 @@
 var game = {
-    user: '',
-    computer: '',
-    userSign: '',
-    computerSign: '',
-    currentPlayer: '',
-    lots: [0,1,2,3,4,5,6,7,8],
+    user: '',        // store user's font-awesome
+    computer: '',    // store computer's font-awesome
+    userSign: '',    // store user's modal (X or O)
+    computerSign: '',// store computer's modal (X or O)
+    currentPlayer: '',// current player, user or computer
+    lots: [0,1,2,3,4,5,6,7,8],   // empty lots available
     squares: ["first", "second", "third", "fourth",
-              "fifth", "sixth", "seventh", "eigth", "ninth"],
-    squareSign: ["E", "E", "E", "E", "E", "E", "E", "E", "E"],
+              "fifth", "sixth", "seventh", "eigth", "ninth"], // id of squares
+    squareSign: ["E", "E", "E", "E", "E", "E", "E", "E", "E"],// status of squares, empty or X or O
+
+    // DOM
+    signDom: null,
+    selectDom: null,
+    resetDom: null,
+    boardDom: null,
+
+    resetGame: function() {
+        var self = this;
+
+        // reset varialbes
+        this.user = '';
+        this.computer = '';
+        this.userSign = '';
+        this.computerSign = '';
+        this.currentPlayer = '';
+        this.lots = [0,1,2,3,4,5,6,7,8];
+        this.squares = ["first", "second", "third", "fourth",
+                  "fifth", "sixth", "seventh", "eigth", "ninth"];
+        this.squareSign = ["E", "E", "E", "E", "E", "E", "E", "E", "E"];
+
+        // clear board
+        Array.from(document.getElementsByClassName("square")).forEach(function(obj) {
+            obj.innerHTML = "";
+        });
+
+        // rebind click event
+        Array.from(document.getElementsByClassName("square")).forEach(function(obj) {
+            obj.onclick = this.userMove.bind(this);
+        }, this);
+
+        // start from select fig
+        this.boardDom.style.display = "none";
+        this.resetDom.style.visibility = "hidden";
+        this.signDom.style.display = "block";
+    },
 
     setFig: function(event) {
       var id = event.target.parentElement.id;
@@ -22,14 +58,14 @@ var game = {
         this.userSign = 'O';
         this.computerSign = 'X';
       }
-      document.getElementById("sign").style.display = "none";
-      document.getElementById("select").style.display = "block";
+      this.signDom.style.display = "none";
+      this.selectDom.style.display = "block";
     },
 
     selectFirst: function(event) {
         var id = event.target.id;
-        document.getElementById("select").style.display = "none";
-        document.getElementById("board").style.display = "block";
+        this.selectDom.style.display = "none";
+        this.boardDom.style.display = "block";
         if (id === "computer") {
             this.currentPlayer = "computer";
             this.computerMove();
@@ -41,6 +77,7 @@ var game = {
 
     gameOver: function() {
         document.getElementById("gameover").innerHTML = "<h1>"+this.result+"</h1>";
+        this.resetDom.style.visibility = "visible";
     },
 
     userMove: function(event) {
@@ -52,7 +89,7 @@ var game = {
         this.lots.splice(this.lots.indexOf(index), 1);
         this.currentPlayer = "computer";
         /*
-        document.getElementById("U").style.visibility = "invisible";
+        document.getElementById("U").style.visibility = "hidden";
         document.getElementById("C").style.visibility = "visible";
         */
         if (!this.isTerminal()) {
@@ -72,7 +109,7 @@ var game = {
         this.lots.splice(x,1);
         this.currentPlayer = "user";
         /*
-        document.getElementById("C").style.visibility = "invisible";
+        document.getElementById("C").style.visibility = "hidden";
         document.getElementById("U").style.visibility = "visible";
         */
         if (this.isTerminal()) {
@@ -120,16 +157,22 @@ var game = {
     },
 
     init: function() {
-        var self = this;
+        //var self = this;
+        this.signDom = document.getElementById("sign");
+        this.selectDom = document.getElementById("select");
+        this.resetDom = document.getElementsByClassName("reset")[0];
+        this.boardDom = document.getElementById("board");
+
         Array.from(document.getElementsByTagName("button")).forEach(function(obj) {
             obj.onclick = this.setFig.bind(this);
-        }, self);
+        }, this);
         Array.from(document.getElementsByClassName("fa")).forEach(function(obj) {
             obj.onclick = this.selectFirst.bind(this);
-        }, self);
+        }, this);
         Array.from(document.getElementsByClassName("square")).forEach(function(obj) {
             obj.onclick = this.userMove.bind(this);
-        }, self);
+        }, this);
+        document.getElementById("restart").onclick = this.resetGame.bind(this);
     }
 };
 
