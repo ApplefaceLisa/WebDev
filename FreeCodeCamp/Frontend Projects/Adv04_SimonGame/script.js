6317adca730f61b4cbc8d4f8ca990cf78f77e15f
ApplefaceLisa <applefacelisa@gmail.com>
2017-03-29 10:42:31 -0700

    Update script.js

--- FreeCodeCamp/Frontend Projects/Adv04_SimonGame/script.js
+++ FreeCodeCamp/Frontend Projects/Adv04_SimonGame/script.js
@@ -1,61 +1,49 @@
-var simon = {
-    switch: false,    // off/on status
-    started: false,   // start/stop status
-    strict: false,    // mode
-    count: 0,         // count
-    countDown: 0,
-    timer: null,
-    player: "simon",  // "simon" or "user"
-    key: ["green", "red", "yellow", "blue"],
-    Keys: [],         // keys to be played by simon
-
+var Simon = {
+    switchOn: false,         // switch on / off
+    started: false,          // start / stop
+    strict: false,           // strict mode on / off
+    count: 0,
     switchDom: null,
     startDom: null,
     strictDom: null,
     countDom: null,
 
     init: function() {
-        this.switchDom = document.getElementById("switch");
+        this.switchDom = document.getElementById("pwr-sw");
         this.startDom = document.getElementById("start");
         this.strictDom = document.getElementById("strict");
-        this.countDom = document.getElementById("count");
-        this.count = 2;
+        this.countDom = document.querySelector(".count");
 
-        this.simonPlay();
-    },
+        this.switchDom.addEventListener("click", function() {
+            this.classList.toggle('sw-on');
+            Simon.switchOn = !Simon.switchOn;      // toggle switch
+            if (!Simon.switchOn) {
+                Simon.started = false;
+                Simon.strict = false;
+                Simon.startDom.classList.remove("red-on");
+                Simon.strictDom.classList.remove("yellow-on");
+                Simon.countDom.classList.add("led-off");
+            } else {
+                Simon.countDom.classList.remove("led-off");
+            }
+        });
 
-    simonPlay: function() {
-        this.countDown = this.count;
-        this.timer = setTimeout(function run() {
-          var index = Math.floor(Math.random() * simon.key.length);
-          var id = simon.key[index];
-          simon.playKey(id);
-          simon.Keys.push(id);
-          --simon.countDown;
-          if (simon.countDown <= 0) {
-            clearTimeout(simon.timer);
-          } else {
-            simon.timer = setTimeout(run, 1000);
-          }
-        }, 1000);
-    },
+        this.startDom.addEventListener("click", function() {
+            if (Simon.switchOn) {
+                this.classList.toggle("red-on");
+                Simon.started = !Simon.started;    // toggle started
+            }
+        });
 
-    playKey: function(id) {
-        document.getElementById(id).play();
-    },
-
-
+        this.strictDom.addEventListener("click", function() {
+            if (Simon.started) {
+                this.classList.toggle("yellow-on");
+                Simon.strict = !Simon.strict;
+            }
+        })
+    }
 };
 
-
 window.onload = function() {
-  Array.from(document.getElementsByTagName("input")).forEach(function(obj) {
-    obj.onclick = function(event) {
-      var id = event.target.value;
-      document.getElementById(id).play();
-    };
-  });
-
-  simon.init();
-  //setInterval(function(){ document.getElementById("green").play(); }, 3000);
+    Simon.init();
 }
